c4cb4f24004f0b217c619ef066dda143994de266
lakshman <lakshmanmurugesan@gmail.com>
2020-05-31 21:36:51 +0530

    Add first rule base

--- Routes/rules.route.js
+++ Routes/rules.route.js
@@ -21,8 +21,8 @@ rules.route('/rules').get((req, res) => {
 
 function loadUserCredential () {
   return AWS.config.update({
-    accessKeyId: XXX,
-    secretAccessKey: XXX
+    accessKeyId: 'XXX',
+    secretAccessKey: 'XXX'
   });
 }
 
@@ -32,8 +32,35 @@ function loadUserRegion (region) {
   });
 }
 
+function hasIpLocalHost (rule) {
+  return !!(rule.IpRanges && rule.IpRanges.length && rule.IpRanges[0].CidrIp === '0.0.0.0/0');
+}
+
+function hasIpV6LocalHost (rule) {
+  return !!(rule.Ipv6Ranges && rule.Ipv6Ranges.length && rule.Ipv6Ranges[0].CidrIpv6 === '::/0');
+}
+
+function hasSSHPort (rule) {
+  return (rule.FromPort > 0 && rule.ToPort === 22);
+}
+
+function validateRule (region, rules, ruleType) {
+  if (ruleType === 'Unrestricted_SSH_Access') {
+    for(let index=0; index < rules.length; index++) {
+      if (hasIpLocalHost(rules[index]) || hasIpV6LocalHost(rules[index]) || hasSSHPort(rules[index])) {
+        return {
+          status: 'failure',
+          message: 'Security group default allows ingress from 0.0.0.0/0 or ::/0 to port 22',
+          region: region
+        }
+      }
+    }
+  }
+}
+
 rules.route('/services').get(async (req, res) => {
   let inboundRules = [];
+  let response = {};
   loadUserCredential();
   let ec2Region = loadUserRegion('ap-south-1');
   await ec2Region.describeRegions( (regionErr, regionData) => {
@@ -48,10 +75,11 @@ rules.route('/services').get(async (req, res) => {
               console.log('error', securityGroupErr);
             } else {
               if (securityGroupData && securityGroupData.SecurityGroups && securityGroupData.SecurityGroups[0] && securityGroupData.SecurityGroups[0].IpPermissions) {
-                inboundRules.push({
-                  region: region.RegionName ,
-                  rules: securityGroupData.SecurityGroups[0].IpPermissions
-                });
+                // inboundRules.push({
+                //   region: region.RegionName ,
+                //   rules: securityGroupData.SecurityGroups[0].IpPermissions
+                // });
+                response = validateRule(region.RegionName, securityGroupData.SecurityGroups[0].IpPermissions, 'Unrestricted_SSH_Access');
               }
             }
           });
@@ -59,7 +87,7 @@ rules.route('/services').get(async (req, res) => {
       });
     }
   });
-  res.json({'data': inboundRules});
+  res.json({'data': response});
 });
 
 module.exports = rules;
