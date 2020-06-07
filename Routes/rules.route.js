c3bae1eafe7010976d00103b2730de3a70093c2e
lakshman <lakshmanmurugesan@gmail.com>
2020-06-07 17:49:39 +0530

    update schema

--- Routes/rules.route.js
+++ Routes/rules.route.js
@@ -40,28 +40,40 @@ function hasIpV6LocalHost (rule) {
   return !!(rule.Ipv6Ranges && rule.Ipv6Ranges.length && rule.Ipv6Ranges[0].CidrIpv6 === '::/0');
 }
 
-function hasSSHPort (rule) {
-  return (rule.FromPort > 0 && rule.ToPort === 22);
+function inRange(port, min, max) {
+  return ((port-min) * (port-max) <= 0);
 }
 
-let dashboardData = [];
+let meta = [];
 
 async function validateRule (region, rules, ruleType, response, validatedRegionCount) {
   if (ruleType === 'Unrestricted_SSH_Access') {
     for(let index=0; index < rules.length; index++) {
-      if (hasIpLocalHost(rules[index]) || hasIpV6LocalHost(rules[index]) || hasSSHPort(rules[index])) {
-        dashboardData.push({
+      if (!meta.some((item) => item.region === region) && (hasIpLocalHost(rules[index]) || hasIpV6LocalHost(rules[index]) || inRange(22, rules[index].FromPort, rules[index].ToPort))) {
+        meta.push({
           status: 'failure',
+          failure_introduced: 'Few moments ago',
           message: 'Security group default allows ingress from 0.0.0.0/0 or ::/0 to port 22',
-          region: region
+          region: region,
+          account: 'prod',
+          description: 'default VPC security group',
+          group_id: 'groupid',
+          group_name: 'default',
+          vpc_id: 'vpc_id'
         });
       }
     }
   }
 
   if (validatedRegionCount === 16) {
-    let boardData = dashboardData;
-    dashboardData = [];
+    let boardData = {
+      rule: ruleType,
+      service: 'EC2',
+      categories: ['security'],
+      riskLevel: 'high',
+      counts: 'Failure 1',
+      meta: meta
+    };
     await response.status(200).json({ 'data': boardData });
   }
 }
@@ -85,7 +97,7 @@ async function fetchEC2Services (response) {
   for (let index = 0; index < regions.length; index++) {
     let regionName = regions[index].RegionName;
     let ec2SecurityGroups = loadUserRegion(regionName);
-    ec2SecurityGroups.describeSecurityGroups((securityGroupErr, securityGroupData) => {
+    await ec2SecurityGroups.describeSecurityGroups((securityGroupErr, securityGroupData) => {
       if (securityGroupErr) {
         errorObj = securityGroupErr;
       } else {
