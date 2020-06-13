4f53ed8a7683b581907867f0b0b8c9fecba78f7e
lakshman <lakshmanmurugesan@gmail.com>
2020-06-13 14:38:36 +0530

    Add port conditions

--- Routes/rules.route.js
+++ Routes/rules.route.js
@@ -44,37 +44,52 @@ function inRange(port, min, max) {
   return ((port-min) * (port-max) <= 0);
 }
 
+let boardData = [];
 let meta = [];
 
-async function validateRule (region, rules, ruleType, response, validatedRegionCount) {
+async function validateRule (region, rules, ruleType, response, validatedRegionCount, validatedRulesCount) {
+  let port;
   if (ruleType === 'Unrestricted_SSH_Access') {
-    for(let index=0; index < rules.length; index++) {
-      if (!meta.some((item) => item.region === region) && (hasIpLocalHost(rules[index]) || hasIpV6LocalHost(rules[index]) || inRange(22, rules[index].FromPort, rules[index].ToPort))) {
-        meta.push({
-          status: 'failure',
-          failure_introduced: 'Few moments ago',
-          message: 'Security group default allows ingress from 0.0.0.0/0 or ::/0 to port 22',
-          region: region,
-          account: 'prod',
-          description: 'default VPC security group',
-          group_id: 'groupid',
-          group_name: 'default',
-          vpc_id: 'vpc_id'
-        });
-      }
+    port = 22;
+  } else if (ruleType === 'Unrestricted_RDP_Access') {
+    port = 3389;
+  } else if (ruleType === 'Unrestricted_Oracle_Access') {
+    port = 1521;
+  }
+
+  for(let index=0; index < rules.length; index++) {
+    if ((hasIpLocalHost(rules[index]) || hasIpV6LocalHost(rules[index]) || inRange(port, rules[index].FromPort, rules[index].ToPort))) {
+      meta.push({
+        status: 'failure',
+        failure_introduced: 'Few moments ago',
+        message: `Security group default allows ingress from 0.0.0.0/0 or ::/0 to port ${port}`,
+        region: region,
+        account: 'prod',
+        description: 'default VPC security group',
+        group_id: 'groupid',
+        group_name: 'default',
+        vpc_id: 'vpc_id'
+      });
     }
   }
 
+
   if (validatedRegionCount === 16) {
-    let boardData = {
+    boardData.push({
       rule: ruleType,
       service: 'EC2',
       categories: ['security'],
       riskLevel: 'high',
       counts: 'Failure 1',
       meta: meta
-    };
-    await response.status(200).json({ 'data': boardData });
+    });
+    meta = [];
+  }
+
+  if (validatedRegionCount === 16 && validatedRulesCount === 3) {
+    let boardSchema = boardData;
+    boardData = [];
+    await response.status(200).json({ 'data': boardSchema });
   }
 }
 
@@ -92,20 +107,24 @@ async function fetchEC2Services (response) {
   }).promise();
 
   // Loop all the regions, get securityGroup details, validate with specific rules
-  const regions = allRegions.Regions;
-  let validatedRegionCount = 1;
-  for (let index = 0; index < regions.length; index++) {
-    let regionName = regions[index].RegionName;
-    let ec2SecurityGroups = loadUserRegion(regionName);
-    await ec2SecurityGroups.describeSecurityGroups((securityGroupErr, securityGroupData) => {
-      if (securityGroupErr) {
-        errorObj = securityGroupErr;
-      } else {
-        if (securityGroupData && securityGroupData.SecurityGroups && securityGroupData.SecurityGroups[0] && securityGroupData.SecurityGroups[0].IpPermissions) {
-          validateRule(regionName, securityGroupData.SecurityGroups[0].IpPermissions, 'Unrestricted_SSH_Access', response, validatedRegionCount++);
+  let rules = ['Unrestricted_SSH_Access', 'Unrestricted_RDP_Access', 'Unrestricted_Oracle_Access'];
+  for (let ruleIndex = 0; ruleIndex < rules.length; ruleIndex++) {
+    const regions = allRegions.Regions;
+    let validatedRegionCount = 1;
+    let validatedRulesCount = ruleIndex+1;
+    for (let index = 0; index < regions.length; index++) {
+      let regionName = regions[index].RegionName;
+      let ec2SecurityGroups = loadUserRegion(regionName);
+      await ec2SecurityGroups.describeSecurityGroups((securityGroupErr, securityGroupData) => {
+        if (securityGroupErr) {
+          errorObj = securityGroupErr;
+        } else {
+          if (securityGroupData && securityGroupData.SecurityGroups && securityGroupData.SecurityGroups[0] && securityGroupData.SecurityGroups[0].IpPermissions) {
+            validateRule(regionName, securityGroupData.SecurityGroups[0].IpPermissions, rules[ruleIndex], response, validatedRegionCount++, validatedRulesCount);
+          }
         }
-      }
-    });
+      });
+    }
   }
 }
 
